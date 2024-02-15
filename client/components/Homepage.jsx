ec25549e1412148b3399be1730c1ae0c2d311c92
julesdmai <julesdmai@gmail.com>
2024-02-14 21:19:05 -0800

    Working on tables in homepage

--- client/components/Homepage.jsx
+++ client/components/Homepage.jsx
@@ -8,6 +8,33 @@ export default function Homepage() {
   return(
     <>
       <h1>Welcome to the homepage</h1>
+      <table>
+        <thead>Medication Schedule</thead>
+        <tr>
+            <th></th>
+            <th>Morning</th>
+            <th>Afternoon</th>
+            <th>Evening</th>
+        </tr>
+        <tr>
+            <td>Medication 1</td>
+            <td><input type="checkbox" name="med1morning"/></td>
+            <td><input type="checkbox" name="med1afternoon"/></td>
+            <td><input type="checkbox" name="med1evening"/></td>
+        </tr>
+        <tr>
+            <td>Medication 2</td>
+            <td><input type="checkbox" name="med1morning"/></td>
+            <td><input type="checkbox" name="med1afternoon"/></td>
+            <td><input type="checkbox" name="med1evening"/></td>
+        </tr>
+        <tr>
+            <td>Medication 3</td>
+            <td><input type="checkbox" name="med1morning"/></td>
+            <td><input type="checkbox" name="med1afternoon"/></td>
+            <td><input type="checkbox" name="med1evening"/></td>
+        </tr>
+      </table>
       <button id="add_med_btn">add new medication</button>
     </>
   );
