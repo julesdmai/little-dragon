b0c4776addebec0cd1c9eb8f265d6ff27453c14b
julesdmai <julesdmai@gmail.com>
2024-02-15 14:40:00 -0800

    Implemented blurred borders

--- client/components/AddMedicationForm.jsx
+++ client/components/AddMedicationForm.jsx
@@ -2,14 +2,17 @@ import React, {useState} from 'react'
 
 export default function AddMedicationForm() {
   return (
+    <>
     <div className="addMedicationContainer">
-        <h2>Add New Medication</h2>
+        <h2>Add Medication</h2>
         <form className="horizontalForm">
             <input type="text" placeholder="name of medication"/>
             <input type="number" placeholder="# per day"/>
-            <input type="number" placeholder="times of the day"/>
+            <input type="number" placeholder="times of the day"/>  
             <button type="submit" className="btnAccount">Add</button>
         </form>
+        {/* <button type="submit" className="btnAccount">Add</button> */}
     </div>
+    </>
   )
 }
